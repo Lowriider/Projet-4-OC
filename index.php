9504785ee332e0ba760fdc1c70296182aaba6206
Lowriider <lowriider23@gmail.com>
2020-06-22 18:15:36 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Blog Écrivain</title>
+</head>
+<body>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#"> Blog d'Écrivain</a>
+              </div>
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+              </ul>
+            </div>
+          </nav>
+
+		        <div id="navbar" class="navbar-collapse collapse">
+
+		        	
+			          	<form class="navbar-form navbar-right" method="POST" >
+			          					            	<div class="form-group">
+			             	 	<input name="login" type="text" id="login" placeholder="Identifiant" class="form-control">
+			            	</div>
+			            	<div class="form-group">
+			              		<input type="password" name="pass" id="pass" placeholder="Mot de passe" class="form-control">
+			            	</div>
+			            	<button type="submit" class="btn btn-success">Connexion</button>
+			          	</form>
+
+			        		        </div>
+      		</div>
+   		</nav>
+
+        
+<div class="jumbotron">
+  	<div class="container">
+        <h1>Bonjour tout le monde !</h1>
+        <p>Bienvenue ! Je suis Jean Forteroche, écrivain. Vous êtes actuellement sur mon Blog où je poste au fur et à mesure un nouveau chapitre de mon dernier roman intitulé "Billet simple pour l'Alaska". N'hésitez pas à y laisser des commentaires !</p>
+  	</div>
+</div>
+
+<div class="container">
+
+		    <div class="panel panel-primary">
+	  		<div class="panel-heading">
+	    		<h3 class="panel-title">
+	    		Lorem ipsum dolor, sit 
+	        	</h3>
+	  		</div>
+	  		<div class="panel-body">
+	  			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum similique tempore accusamus suscipit molestiae? Repudiandae, perferendis aliquam! Rerum enim, quos deserunt iure blanditiis nisi ex aliquam qui odio nemo magnam?<p>[...]</p>
+	  		</div>
+	  		<div class="panel-footer">
+	  			<a href="index.php?action=post&amp;id=5">
+					<p class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-comment"></span></p>
+				</a>
+			</div>
+		</div>
+
+		    <div class="panel panel-primary">
+	  		<div class="panel-heading">
+	    		<h3 class="panel-title">
+	    			Lorem ipsum, dolor sit 
+	  		</div>
+	  		<div class="panel-body">
+	  			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nam quia nisi debitis exercitationem quae porro accusantium commodi eius fuga aliquid modi cum non voluptate ullam enim ipsa fugit? Quo.<p>[...]</p>
+	  		</div>
+	  		<div class="panel-footer">
+	  			<a href="index.php?action=post&amp;id=4">
+					<p class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-comment"></span></p>
+				</a>
+			</div>
+		</div>
+
+		    <div class="panel panel-primary">
+	  		<div class="panel-heading">
+	    		<h3 class="panel-title">
+	    			Lorem ipsum dolor            	
+	        	</h3>
+	  		</div>
+	  		<div class="panel-body">
+	  			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam reiciendis consequuntur explicabo numquam, in maxime, fugiat voluptatum necessitatibus aliquam sed nihil molestiae. Ea, impedit. Earum eaque iusto nobis ipsum in!<p>[...]</p>
+	  		</div>
+	  		<div class="panel-footer">
+	  			<a href="index.php?action=post&amp;id=2">
+					<p class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-comment"></span></p>
+				</a>
+			</div>
+		</div>
+
+		    <div class="panel panel-primary">
+	  		<div class="panel-heading">
+	    		<h3 class="panel-title">
+	    			Lorem ipsum, dolor sit amet
+	        	</h3>
+	  		</div>
+	  		<div class="panel-body">
+	  			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum corrupti id sequi voluptas accusamus incidunt quam dolor voluptatibus quo, tempora obcaecati possimus enim aperiam quia quas sint ullam. Commodi, provident!</p>
+	  		</div>
+	  		<div class="panel-footer">
+	  			<a href="index.php?action=post&amp;id=1">
+					<p class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-comment"></span></p>
+				</a>
+			</div>
+		</div>
+
+	
+</div>
+
+
+        <footer class="container">
+        	<p>Tous droits réservés © - Formation Openclassrooms - Développeur Web Junior - Projet n°4 : Créez un blog pour un écrivain</p>
+        </footer>
+        <script src="bootstrap.min.js"></script>
+</body>
+</html>
